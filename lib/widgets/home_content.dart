946e96a478a3e8fbc75ee86bb2aebddc766d9583
Journii <ginopaflo001608@gmail.com>
2023-10-12 14:04:28 -0500

    app bar

---
+++ lib/widgets/home_content.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Cooking with the Power of the Sun'));
+  }
+}
